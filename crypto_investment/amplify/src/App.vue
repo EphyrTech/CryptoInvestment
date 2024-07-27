ebc6b51fa80640fcb5a22cd60b5f35231dc00661
bvlesiv <bohdan.volodymyr@healthforce.ai>
2024-07-27 16:09:25 +0200

    added amplify vue

---
+++ crypto_investment/amplify/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <InvestmentPlot />
+  </div>
+</template>
+
+<script>
+import InvestmentPlot from './components/InvestmentPlot.vue';
+
+export default {
+  name: 'App',
+  components: {
+    InvestmentPlot
+  }
+};
+</script>
---
+++ crypto_investment/amplify/src/components/InvestmentPlot.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Investment Value Over Time</h1>
+    <img :src="imageUrl" alt="Investment Plot">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrl: 'https://crypto-visualizations.s3.amazonaws.com/investment_plot.png'
+    };
+  }
+};
+</script>
+
+<style scoped>
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
