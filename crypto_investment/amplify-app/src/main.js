5db86d90999fd6a4444fbb1bda56e450293ec7ac
bvlesiv <bohdan.volodymyr@healthforce.ai>
2024-07-27 18:13:12 +0200

    change front

--- crypto_investment/amplify-app/src/main.js
+++ crypto_investment/amplify-app/src/main.js
@@ -1,3 +1,4 @@
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
